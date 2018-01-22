2c770c6959bc84630fba784e182697c2c5069d82
cclauss <cclauss@bluewin.ch>
2018-01-22 10:31:16 +0100

    from six.moves import xrange
    __xrange()__ was removed from Python 3 in favor of __range()__.

--- cloud_tpu/models/densenet/densenet_model.py
+++ cloud_tpu/models/densenet/densenet_model.py
@@ -21,6 +21,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+from six.moves import xrange
 import tensorflow as tf
 
 # Learning hyperaparmeters
