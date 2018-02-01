cd7c86b74f91f73df605bf6675a3e05406c38184
cclauss <cclauss@bluewin.ch>
2018-02-01 11:17:46 +0100

    from six.moves import xrange for line 257

--- models/experimental/inception/inception_v3_old.py
+++ models/experimental/inception/inception_v3_old.py
@@ -21,6 +21,8 @@ from __future__ import print_function
 
 import os
 
+from six.moves import xrange  # pylint: disable=redefined-builtin
+
 import tensorflow as tf
 
 from tensorflow.contrib import slim
