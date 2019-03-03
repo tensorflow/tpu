baadcb5d1f92b06ab6f73317520acc14f3038768
liammdalton <liam.m.dalton@gmail.com>
2019-03-02 21:48:57 -0800

    Add hyperparameter framework to ResNet using python dicts. (#294)

--- models/official/resnet/lars_util.py
+++ models/official/resnet/lars_util.py
@@ -25,26 +25,26 @@ FLAGS = flags.FLAGS
 
 
 def poly_rate_schedule(current_epoch,
-                       poly_rate=0.0):
+                       params):
   """Handles linear scaling rule, gradual warmup, and LR decay.
 
   The learning rate starts at 0, then it increases linearly per step.  After
-  FLAGS.poly_warmup_epochs, we reach the base learning rate (scaled to account
+  poly_warmup_epochs, we reach the base learning rate (scaled to account
   for batch size). The learning rate is then decayed using a polynomial rate
   decay schedule with power 2.0.
 
   Args:
     current_epoch: `Tensor` for current epoch.
-    poly_rate: Polynomial decay rate.
+    params: Parameter set for Resnet model. 
 
   Returns:
     A scaled `Tensor` for current learning rate.
   """
 
-  if FLAGS.train_batch_size <= 16384:
+  if params['train_batch_size'] <= 16384:
     plr = 25.0
     w_epochs = 5
-  elif FLAGS.train_batch_size == 32768:
+  elif params['train_batch_size'] == 32768:
     plr = 34.0
     w_epochs = 12
   else:
@@ -52,30 +52,30 @@ def poly_rate_schedule(current_epoch,
     w_epochs = 16
 
   # Override default poly learning rate and warmup epochs
-  if poly_rate > 0.0:
-    plr = poly_rate
+  if params['poly_rate'] > 0.0:
+    plr = params['poly_rate']
 
   wrate = (plr * current_epoch / w_epochs)
-  w_steps = (w_epochs * FLAGS.num_train_images // FLAGS.train_batch_size)
+  w_steps = (w_epochs * params['num_train_images'] // params['train_batch_size'])
   min_step = tf.constant(1, dtype=tf.int64)
   global_step = tf.train.get_or_create_global_step()
   decay_steps = tf.maximum(min_step, tf.subtract(global_step, w_steps))
   poly_rate = tf.train.polynomial_decay(
       plr,
       decay_steps,
-      FLAGS.train_steps - w_steps + 1,
+      params['train_steps'] - w_steps + 1,
       power=2.0)
   decay_rate = tf.where(current_epoch <= w_epochs, wrate, poly_rate)
   return decay_rate
 
 
-def init_lars_optimizer(current_epoch):
+def init_lars_optimizer(current_epoch, params):
   """Initialize the LARS Optimizer."""
 
-  learning_rate = poly_rate_schedule(current_epoch, FLAGS.poly_rate)
+  learning_rate = poly_rate_schedule(current_epoch, params)
   optimizer = tf.contrib.opt.LARSOptimizer(
       learning_rate,
-      momentum=FLAGS.momentum,
-      weight_decay=FLAGS.weight_decay,
+      momentum=params['momentum'],
+      weight_decay=params['weight_decay'],
       skip_list=['batch_normalization', 'bias'])
   return optimizer
