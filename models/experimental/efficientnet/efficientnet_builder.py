071665d0f771551d4d86044d95de532fb269fd56
saberkun <solitarylord@gmail.com>
2019-05-14 14:54:32 -0700

    Merge internal changes into public repository (change 248216613)

---
+++ models/experimental/efficientnet/efficientnet_builder.py
@@ -0,0 +1,176 @@
+# Copyright 2019 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Model Builder for EfficientNet."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import re
+import tensorflow as tf
+
+import efficientnet_model
+
+
+def efficientnet_params(model_name):
+  """Get efficientnet params based on model name."""
+  params_dict = {
+      # (width_coefficient, depth_coefficient, resolution)
+      'efficientnet-b0': (1.0, 1.0, 224),
+      'efficientnet-b1': (1.0, 1.1, 240),
+      'efficientnet-b2': (1.1, 1.2, 260),
+      'efficientnet-b3': (1.2, 1.4, 300),
+      'efficientnet-b4': (1.4, 1.8, 380),
+      'efficientnet-b5': (1.6, 2.2, 456),
+      'efficientnet-b6': (1.8, 2.6, 528),
+      'efficientnet-b7': (2.0, 3.1, 600),
+  }
+  return params_dict[model_name]
+
+
+class BlockDecoder(object):
+  """Block Decoder for readability."""
+
+  def _decode_block_string(self, block_string):
+    """Gets a block through a string notation of arguments."""
+    assert isinstance(block_string, str)
+    ops = block_string.split('_')
+    options = {}
+    for op in ops:
+      splits = re.split(r'(\d.*)', op)
+      if len(splits) >= 2:
+        key, value = splits[:2]
+        options[key] = value
+
+    if 's' not in options or len(options['s']) != 2:
+      raise ValueError('Strides options should be a pair of integers.')
+
+    return efficientnet_model.BlockArgs(
+        kernel_size=int(options['k']),
+        num_repeat=int(options['r']),
+        input_filters=int(options['i']),
+        output_filters=int(options['o']),
+        expand_ratio=int(options['e']),
+        id_skip=('noskip' not in block_string),
+        se_ratio=float(options['se']) if 'se' in options else None,
+        strides=[int(options['s'][0]), int(options['s'][1])])
+
+  def _encode_block_string(self, block):
+    """Encodes a block to a string."""
+    args = [
+        'r%d' % block.num_repeat,
+        'k%d' % block.kernel_size,
+        's%d%d' % (block.strides[0], block.strides[1]),
+        'e%s' % block.expand_ratio,
+        'i%d' % block.input_filters,
+        'o%d' % block.output_filters
+    ]
+    if block.se_ratio > 0 and block.se_ratio <= 1:
+      args.append('se%s' % block.se_ratio)
+    if block.id_skip is False:
+      args.append('noskip')
+    return '_'.join(args)
+
+  def decode(self, string_list):
+    """Decodes a list of string notations to specify blocks inside the network.
+
+    Args:
+      string_list: a list of strings, each string is a notation of block.
+
+    Returns:
+      A list of namedtuples to represent blocks arguments.
+    """
+    assert isinstance(string_list, list)
+    blocks_args = []
+    for block_string in string_list:
+      blocks_args.append(self._decode_block_string(block_string))
+    return blocks_args
+
+  def encode(self, blocks_args):
+    """Encodes a list of Blocks to a list of strings.
+
+    Args:
+      blocks_args: A list of namedtuples to represent blocks arguments.
+    Returns:
+      a list of strings, each string is a notation of block.
+    """
+    block_strings = []
+    for block in blocks_args:
+      block_strings.append(self._encode_block_string(block))
+    return block_strings
+
+
+def efficientnet(width_coefficient=None, depth_coefficient=None):
+  """Creates a efficientnet-b0 model."""
+  blocks_args = [
+      'r1_k3_s11_e1_i32_o16_se0.25', 'r2_k3_s22_e6_i16_o24_se0.25',
+      'r2_k5_s22_e6_i24_o40_se0.25', 'r3_k3_s22_e6_i40_o80_se0.25',
+      'r3_k5_s11_e6_i80_o112_se0.25', 'r4_k5_s22_e6_i112_o192_se0.25',
+      'r1_k3_s11_e6_i192_o320_se0.25',
+  ]
+  global_params = efficientnet_model.GlobalParams(
+      batch_norm_momentum=0.99,
+      batch_norm_epsilon=1e-3,
+      dropout_rate=0.2,
+      drop_connect_rate=0.3,
+      data_format='channels_last',
+      num_classes=1000,
+      width_coefficient=width_coefficient,
+      depth_coefficient=depth_coefficient,
+      depth_divisor=8,
+      min_depth=None)
+  decoder = BlockDecoder()
+  return decoder.decode(blocks_args), global_params
+
+
+def build_model(images, model_name, training, override_params=None):
+  """A helper functiion to creates a model and returns predicted logits.
+
+  Args:
+    images: input images tensor.
+    model_name: string, the predefined model name.
+    training: boolean, whether the model is constructed for training.
+    override_params: A dictionary of params for overriding. Fields must exist in
+      efficientnet_model.GlobalParams.
+
+  Returns:
+    logits: the logits tensor of classes.
+    endpoints: the endpoints for each layer.
+  Raises:
+    When model_name specified an undefined model, raises NotImplementedError.
+    When override_params has invalid fields, raises ValueError.
+  """
+  assert isinstance(images, tf.Tensor)
+  if model_name.startswith('efficientnet'):
+    width_coefficient, depth_coefficient, _ = efficientnet_params(model_name)
+    blocks_args, global_params = efficientnet(width_coefficient,
+                                              depth_coefficient)
+  else:
+    raise NotImplementedError('model name is not pre-defined: %s' % model_name)
+
+  if override_params:
+    # ValueError will be raised here if override_params has fields not included
+    # in global_params.
+    global_params = global_params._replace(**override_params)
+
+  tf.logging.info('global_params= %s', global_params)
+  tf.logging.info('blocks_args= %s', blocks_args)
+
+  with tf.variable_scope(model_name):
+    model = efficientnet_model.Model(blocks_args, global_params)
+    logits = model(images, training=training)
+
+  logits = tf.identity(logits, 'logits')
+  return logits, model.endpoints
