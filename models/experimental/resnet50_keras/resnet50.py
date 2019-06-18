Fix the metric computation and return the top_k output.
PiperOrigin-RevId: 253889875

models/experimental/resnet50_keras/resnet50.py
@@ -158,9 +158,7 @@ def sparse_top_k_categorical_accuracy(y_true, y_pred, k=5):
     y_true = tf.squeeze(y_true, [-1])
 
   y_true = tf.cast(y_true, 'int32')
-
-  in_top_k_on_device = tf.nn.in_top_k(y_pred, y_true, k)
-  return tf.keras.backend.mean(in_top_k_on_device, axis=-1)
+  return tf.nn.in_top_k(y_pred, y_true, k)
 
 
 def main(unused_argv):