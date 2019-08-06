d12647bb3fdc2612415302aeed5688156b6b8f73
A. Unique TensorFlower <gardener@tensorflow.org>
2019-08-06 10:15:45 -0700

    Simplify ground truth masks generation and improve get_mask_target on TPU
    PiperOrigin-RevId: 261940663

--- models/official/detection/dataloader/tf_example_decoder.py
+++ models/official/detection/dataloader/tf_example_decoder.py
@@ -58,7 +58,7 @@ class TfExampleDecoder(object):
 
   def _decode_image(self, parsed_tensors):
     """Decodes the image and set its static shape."""
-    image = tf.io.decode_image(parsed_tensors['image/encoded'], channels=3)
+    image = tf.io.decode_image(parsed_tensors['image/encoded'])
     image.set_shape([None, None, 3])
     return image
 
