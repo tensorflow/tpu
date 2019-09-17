6fad1a82a358b42e7238b2e2effb42d1fd45115e
A. Unique TensorFlower <gardener@tensorflow.org>
2019-09-17 09:54:25 -0700

    Enable customized 'pre_nms_num_boxes' to specify the number of candidate detections per class before NMS.
    PiperOrigin-RevId: 269591199

--- models/official/detection/configs/retinanet_config.py
+++ models/official/detection/configs/retinanet_config.py
@@ -167,7 +167,8 @@ RETINANET_CFG = {
         'num_classes': 91,
         'max_total_size': 100,
         'nms_iou_threshold': 0.5,
-        'score_threshold': 0.05
+        'score_threshold': 0.05,
+        'pre_nms_num_boxes': 5000,
     },
     'enable_summary': False,
 }
