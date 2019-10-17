d1548906df3940bb570ee2b55a3d4e4471ca44bc
Pengchong Jin <pengchong@google.com>
2019-10-16 17:06:42 -0700

    Rename proposal_ops to sampling_ops.
    PiperOrigin-RevId: 275146533

--- models/official/detection/ops/sampling_ops.py
+++ models/official/detection/ops/sampling_ops.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Proposal-related ops."""
+"""Sampling related ops."""
 
 from __future__ import absolute_import
 from __future__ import division
