2cc8ef543fac7614b8a91e396e3cca5045e3cec6
David <licausedavid@gmail.com>
2019-10-22 13:54:53 -0600

    retinanet - Bug fix for single node CPU/GPU training

--- models/official/retinanet/retinanet_main.py
+++ models/official/retinanet/retinanet_main.py
@@ -353,6 +353,7 @@ def main(argv):
         resnet_checkpoint=FLAGS.resnet_checkpoint,
         val_json_file=FLAGS.val_json_file,
         mode=FLAGS.mode,
+        auto_mixed_precision=FLAGS.auto_mixed_precision,
     )
     tpu_config = tf.contrib.tpu.TPUConfig(
         FLAGS.iterations_per_loop,
