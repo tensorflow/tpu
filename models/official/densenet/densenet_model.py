193c4ee7b629ae551d9a88f37fa9f92d138cfa17
Yanhua Sun <yanhuasun@google.com>
2020-02-10 15:33:55 -0800

    \nInternal refactor\n
    PiperOrigin-RevId: 294318792

--- models/official/densenet/densenet_model.py
+++ models/official/densenet/densenet_model.py
@@ -22,7 +22,7 @@ from __future__ import division
 from __future__ import print_function
 
 from absl import flags
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
 from tensorflow.contrib import layers as contrib_layers
 
 # Learning hyperaparmeters
