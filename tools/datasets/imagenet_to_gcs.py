eeee14a254adc199d5ee7baf002db3d73843fc8b
Allen Wang <allencwang@google.com>
2020-07-20 09:36:38 -0700

    Port `imagenet_to_gcs.py` to Python3.
    PiperOrigin-RevId: 322161598

--- tools/datasets/imagenet_to_gcs.py
+++ tools/datasets/imagenet_to_gcs.py
@@ -33,7 +33,7 @@ To run the script to preprocess the raw dataset as TFRecords and upload to gcs,
 run the following command:
 
 ```
-python imagenet_to_gcs.py \
+python3 imagenet_to_gcs.py \
   --project="TEST_PROJECT" \
   --gcs_output_path="gs://TEST_BUCKET/IMAGENET_DIR" \
   --raw_data_dir="path/to/imagenet"
@@ -44,9 +44,7 @@ python imagenet_to_gcs.py \
 import math
 import os
 import random
-import tarfile
-import urllib
-
+from typing import Iterable, List, Mapping, Union, Tuple
 from absl import app
 from absl import flags
 from absl import logging
@@ -69,11 +67,6 @@ flags.DEFINE_boolean(
 
 FLAGS = flags.FLAGS
 
-BASE_URL = 'http://www.image-net.org/challenges/LSVRC/2012/nnoupb/'
-LABELS_URL = 'https://raw.githubusercontent.com/tensorflow/models/master/research/inception/inception/data/imagenet_2012_validation_synset_labels.txt'  # pylint: disable=line-too-long
-
-TRAINING_FILE = 'ILSVRC2012_img_train.tar'
-VALIDATION_FILE = 'ILSVRC2012_img_val.tar'
 LABELS_FILE = 'synset_labels.txt'
 
 TRAINING_SHARDS = 1024
@@ -83,80 +76,33 @@ TRAINING_DIRECTORY = 'train'
 VALIDATION_DIRECTORY = 'validation'
 
 
-def _check_or_create_dir(directory):
-  """Check if directory exists otherwise create it."""
+def _check_or_create_dir(directory: str):
+  """Checks if directory exists otherwise creates it."""
   if not tf.gfile.Exists(directory):
     tf.gfile.MakeDirs(directory)
 
 
-def download_dataset(raw_data_dir):
-  """Download the Imagenet dataset into the temporary directory."""
-  def _download(url, filename):
-    """Download the dataset at the provided filepath."""
-    urllib.urlretrieve(url, filename)
-
-  def _get_members(filename):
-    """Get all members of a tarfile."""
-    tar = tarfile.open(filename)
-    members = tar.getmembers()
-    tar.close()
-    return members
-
-  def _untar_file(filename, directory, member=None):
-    """Untar a file at the provided directory path."""
-    _check_or_create_dir(directory)
-    tar = tarfile.open(filename)
-    if member is None:
-      tar.extractall(path=directory)
-    else:
-      tar.extract(member, path=directory)
-    tar.close()
-
-  # Check if raw_data_dir exists
-  _check_or_create_dir(raw_data_dir)
-
-  # Download the training data
-  logging.info('Downloading the training set. This may take a few hours.')
-  directory = os.path.join(raw_data_dir, TRAINING_DIRECTORY)
-  filename = os.path.join(raw_data_dir, TRAINING_FILE)
-  _download(BASE_URL + TRAINING_FILE, filename)
-
-  # The training tarball contains multiple tar balls inside it. Extract them
-  # in order to create a clean directory structure.
-  for member in _get_members(filename):
-    subdirectory = os.path.join(directory, member.name.split('.')[0])
-    sub_tarfile = os.path.join(subdirectory, member.name)
-
-    _untar_file(filename, subdirectory, member)
-    _untar_file(sub_tarfile, subdirectory)
-    os.remove(sub_tarfile)
-
-  # Download synset_labels for validation set
-  logging.info('Downloading the validation labels.')
-  _download(LABELS_URL, os.path.join(raw_data_dir, LABELS_FILE))
-
-  # Download the validation data
-  logging.info('Downloading the validation set. This may take a few hours.')
-  directory = os.path.join(raw_data_dir, VALIDATION_DIRECTORY)
-  filename = os.path.join(raw_data_dir, VALIDATION_FILE)
-  _download(BASE_URL + VALIDATION_FILE, filename)
-  _untar_file(filename, directory)
-
-
-def _int64_feature(value):
-  """Wrapper for inserting int64 features into Example proto."""
+def _int64_feature(value: Union[int, Iterable[int]]) -> tf.train.Feature:
+  """Inserts int64 features into Example proto."""
   if not isinstance(value, list):
     value = [value]
   return tf.train.Feature(int64_list=tf.train.Int64List(value=value))
 
 
-def _bytes_feature(value):
-  """Wrapper for inserting bytes features into Example proto."""
+def _bytes_feature(value: Union[bytes, str]) -> tf.train.Feature:
+  """Inserts bytes features into Example proto."""
+  if isinstance(value, str):
+    value = bytes(value, 'utf-8')
   return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
 
 
-def _convert_to_example(filename, image_buffer, label, synset, height, width):
-  """Build an Example proto for an example.
+def _convert_to_example(filename: str,
+                        image_buffer: str,
+                        label: int,
+                        synset: str,
+                        height: int,
+                        width: int) -> tf.train.Example:
+  """Builds an Example proto for an ImageNet example.
 
   Args:
     filename: string, path to an image file, e.g., '/path/to/example.JPG'
@@ -167,6 +113,7 @@ def _convert_to_example(filename, image_buffer, label, synset, height, width):
     width: integer, image width in pixels
   Returns:
     Example proto
+
   """
   colorspace = 'RGB'
   channels = 3
@@ -185,28 +132,30 @@ def _convert_to_example(filename, image_buffer, label, synset, height, width):
   return example
 
 
-def _is_png(filename):
-  """Determine if a file contains a PNG format image.
+def _is_png(filename: str) -> bool:
+  """Determines if a file contains a PNG format image.
 
   Args:
     filename: string, path of the image file.
 
   Returns:
     boolean indicating if the image is a PNG.
+
   """
   # File list from:
   # https://github.com/cytsai/ilsvrc-cmyk-image-list
   return 'n02105855_2933.JPEG' in filename
 
 
-def _is_cmyk(filename):
-  """Determine if file contains a CMYK JPEG format image.
+def _is_cmyk(filename: str) -> bool:
+  """Determines if file contains a CMYK JPEG format image.
 
   Args:
     filename: string, path of the image file.
 
   Returns:
     boolean indicating if the image is a JPEG encoded with CMYK color space.
+
   """
   # File list from:
   # https://github.com/cytsai/ilsvrc-cmyk-image-list
@@ -245,15 +194,18 @@ class ImageCoder(object):
     self._decode_jpeg_data = tf.placeholder(dtype=tf.string)
     self._decode_jpeg = tf.image.decode_jpeg(self._decode_jpeg_data, channels=3)
 
-  def png_to_jpeg(self, image_data):
+  def png_to_jpeg(self, image_data: bytes) -> tf.Tensor:
+    """Converts a PNG compressed image to a JPEG Tensor."""
     return self._sess.run(self._png_to_jpeg,
                           feed_dict={self._png_data: image_data})
 
-  def cmyk_to_rgb(self, image_data):
+  def cmyk_to_rgb(self, image_data: bytes) -> tf.Tensor:
+    """Converts a CMYK image to RGB Tensor."""
     return self._sess.run(self._cmyk_to_rgb,
                           feed_dict={self._cmyk_data: image_data})
 
-  def decode_jpeg(self, image_data):
+  def decode_jpeg(self, image_data: bytes) -> tf.Tensor:
+    """Decodes a JPEG image."""
     image = self._sess.run(self._decode_jpeg,
                            feed_dict={self._decode_jpeg_data: image_data})
     assert len(image.shape) == 3
@@ -261,19 +213,22 @@ class ImageCoder(object):
     return image
 
 
-def _process_image(filename, coder):
-  """Process a single image file.
+def _process_image(
+    filename: str, coder: ImageCoder) -> Tuple[str, int, int]:
+  """Processes a single image file.
 
   Args:
     filename: string, path to an image file e.g., '/path/to/example.JPG'.
     coder: instance of ImageCoder to provide TensorFlow image coding utils.
+
   Returns:
     image_buffer: string, JPEG encoding of RGB image.
     height: integer, image height in pixels.
     width: integer, image width in pixels.
+
   """
   # Read the image file.
-  with tf.gfile.FastGFile(filename, 'r') as f:
+  with tf.gfile.FastGFile(filename, 'rb') as f:
     image_data = f.read()
 
   # Clean the dirty data.
@@ -298,15 +253,21 @@ def _process_image(filename, coder):
   return image_data, height, width
 
 
-def _process_image_files_batch(coder, output_file, filenames, synsets, labels):
-  """Processes and saves list of images as TFRecords.
+def _process_image_files_batch(
+    coder: ImageCoder,
+    output_file: str,
+    filenames: Iterable[str],
+    synsets: Iterable[Union[str, bytes]],
+    labels: Mapping[str, int]):
+  """Processes and saves a list of images as TFRecords.
 
   Args:
     coder: instance of ImageCoder to provide TensorFlow image coding utils.
-    output_file: string, unique identifier specifying the data set
-    filenames: list of strings; each string is a path to an image file
-    synsets: list of strings; each string is a unique WordNet ID
-    labels: map of string to integer; id for all synset labels
+    output_file: string, unique identifier specifying the data set.
+    filenames: list of strings; each string is a path to an image file.
+    synsets: list of strings; each string is a unique WordNet ID.
+    labels: map of string to integer; id for all synset labels.
+
   """
   writer = tf.python_io.TFRecordWriter(output_file)
 
@@ -320,20 +281,26 @@ def _process_image_files_batch(coder, output_file, filenames, synsets, labels):
   writer.close()
 
 
-def _process_dataset(filenames, synsets, labels, output_directory, prefix,
-                     num_shards):
+def _process_dataset(
+    filenames: Iterable[str],
+    synsets: Iterable[str],
+    labels: Mapping[str, int],
+    output_directory: str,
+    prefix: str,
+    num_shards: int) -> List[str]:
   """Processes and saves list of images as TFRecords.
 
   Args:
-    filenames: list of strings; each string is a path to an image file
-    synsets: list of strings; each string is a unique WordNet ID
-    labels: map of string to integer; id for all synset labels
-    output_directory: path where output files should be created
-    prefix: string; prefix for each file
-    num_shards: number of chucks to split the filenames into
+    filenames: iterable of strings; each string is a path to an image file.
+    synsets: iterable of strings; each string is a unique WordNet ID.
+    labels: map of string to integer; id for all synset labels.
+    output_directory: path where output files should be created.
+    prefix: string; prefix for each file.
+    num_shards: number of chunks to split the filenames into.
 
   Returns:
     files: list of tf-record filepaths created from processing the dataset.
+
   """
   _check_or_create_dir(output_directory)
   chunksize = int(math.ceil(len(filenames) / num_shards))
@@ -345,7 +312,7 @@ def _process_dataset(filenames, synsets, labels, output_directory, prefix,
     chunk_files = filenames[shard * chunksize : (shard + 1) * chunksize]
     chunk_synsets = synsets[shard * chunksize : (shard + 1) * chunksize]
     output_file = os.path.join(
-        output_directory, '%s-%.5d-of-%.5d', (prefix, shard, num_shards))
+        output_directory, '%s-%.5d-of-%.5d' % (prefix, shard, num_shards))
     _process_image_files_batch(coder, output_file, chunk_files,
                                chunk_synsets, labels)
     logging.info('Finished writing file: %s', output_file)
@@ -353,14 +320,16 @@ def _process_dataset(filenames, synsets, labels, output_directory, prefix,
   return files
 
 
-def convert_to_tf_records(raw_data_dir):
-  """Convert the Imagenet dataset into TF-Record dumps."""
+def convert_to_tf_records(
+    raw_data_dir: str,
+    local_scratch_dir: str) -> Tuple[List[str], List[str]]:
+  """Converts the Imagenet dataset into TF-Record dumps."""
 
   # Shuffle training records to ensure we are distributing classes
   # across the batches.
   random.seed(0)
   def make_shuffle_idx(n):
-    order = range(n)
+    order = list(range(n))
     random.shuffle(order)
     return order
 
@@ -371,6 +340,7 @@ def convert_to_tf_records(raw_data_dir):
   # Get training file synset labels from the directory name
   training_synsets = [
       os.path.basename(os.path.dirname(f)) for f in training_files]
+  training_synsets = list(map(lambda x: bytes(x, 'utf-8'), training_synsets))
 
   training_shuffle_idx = make_shuffle_idx(len(training_files))
   training_files = [training_files[i] for i in training_shuffle_idx]
@@ -382,7 +352,7 @@ def convert_to_tf_records(raw_data_dir):
 
   # Get validation file synset labels from labels.txt
   validation_synsets = tf.gfile.FastGFile(
-      os.path.join(raw_data_dir, LABELS_FILE), 'r').read().splitlines()
+      os.path.join(raw_data_dir, LABELS_FILE), 'rb').read().splitlines()
 
   # Create unique ids for all synsets
   labels = {v: k + 1 for k, v in enumerate(
@@ -392,24 +362,28 @@ def convert_to_tf_records(raw_data_dir):
   logging.info('Processing the training data.')
   training_records = _process_dataset(
       training_files, training_synsets, labels,
-      os.path.join(FLAGS.local_scratch_dir, TRAINING_DIRECTORY),
+      os.path.join(local_scratch_dir, TRAINING_DIRECTORY),
       TRAINING_DIRECTORY, TRAINING_SHARDS)
 
   # Create validation data
   logging.info('Processing the validation data.')
   validation_records = _process_dataset(
       validation_files, validation_synsets, labels,
-      os.path.join(FLAGS.local_scratch_dir, VALIDATION_DIRECTORY),
+      os.path.join(local_scratch_dir, VALIDATION_DIRECTORY),
       VALIDATION_DIRECTORY, VALIDATION_SHARDS)
 
   return training_records, validation_records
 
 
-def upload_to_gcs(training_records, validation_records):
-  """Upload TF-Record files to GCS, at provided path."""
+def upload_to_gcs(training_records: Iterable[str],
+                  validation_records: Iterable[str],
+                  gcs_output_path: str,
+                  gcs_project: str,
+                  client: storage.Client = None):
+  """Uploads TF-Record files to GCS, at provided path."""
 
   # Find the GCS bucket_name and key_prefix for dataset files
-  path_parts = FLAGS.gcs_output_path[5:].split('/', 1)
+  path_parts = gcs_output_path[5:].split('/', 1)
   bucket_name = path_parts[0]
   if len(path_parts) == 1:
     key_prefix = ''
@@ -418,11 +392,11 @@ def upload_to_gcs(training_records, validation_records):
   else:
     key_prefix = path_parts[1] + '/'
 
-  client = storage.Client(project=FLAGS.project)
+  client = client if client else storage.Client(project=gcs_project)
   bucket = client.get_bucket(bucket_name)
 
-  def _upload_files(filenames):
-    """Upload a list of files into a specifc subdirectory."""
+  def _upload_files(filenames: Iterable[str]):
+    """Uploads a list of files into a specifc subdirectory."""
     for i, filename in enumerate(sorted(filenames)):
       blob = bucket.blob(key_prefix + os.path.basename(filename))
       blob.upload_from_filename(filename)
@@ -438,34 +412,59 @@ def upload_to_gcs(training_records, validation_records):
   _upload_files(validation_records)
 
 
-def main(argv):  # pylint: disable=unused-argument
-  logging.set_verbosity(logging.INFO)
+def run(raw_data_dir: str,
+        gcs_upload: bool,
+        gcs_project: str,
+        gcs_output_path: str,
+        local_scratch_dir: str,
+        client: storage.Client = None):
+  """Runs the ImageNet preprocessing and uploading to GCS.
 
-  if FLAGS.gcs_upload and FLAGS.project is None:
+  Args:
+    raw_data_dir: str, the path to the folder with raw ImageNet data.
+    gcs_upload: bool, whether or not to upload to GCS.
+    gcs_project: str, the GCS project to upload to.
+    gcs_output_path: str, the GCS bucket to write to.
+    local_scratch_dir: str, the local directory path.
+    client: An optional storage client.
+
+  """
+  if gcs_upload and gcs_project is None:
     raise ValueError('GCS Project must be provided.')
 
-  if FLAGS.gcs_upload and FLAGS.gcs_output_path is None:
+  if gcs_upload and gcs_output_path is None:
     raise ValueError('GCS output path must be provided.')
-  elif FLAGS.gcs_upload and not FLAGS.gcs_output_path.startswith('gs://'):
+  elif gcs_upload and not gcs_output_path.startswith('gs://'):
     raise ValueError('GCS output path must start with gs://')
 
-  if FLAGS.local_scratch_dir is None:
-    raise ValueError('Scratch directory path must be provided.')
-
-  # Download the dataset if it is not present locally
-  raw_data_dir = FLAGS.raw_data_dir
   if raw_data_dir is None:
     raise AssertionError(
         'The ImageNet download path is no longer supported. Please download '
-        'the .tar files manually and provide the `raw_data_dir`.')
+        'and extract the .tar files manually and provide the `raw_data_dir`.')
 
   # Convert the raw data into tf-records
-  training_records, validation_records = convert_to_tf_records(raw_data_dir)
+  training_records, validation_records = convert_to_tf_records(
+      raw_data_dir=raw_data_dir,
+      local_scratch_dir=local_scratch_dir)
 
   # Upload to GCS
-  if FLAGS.gcs_upload:
-    upload_to_gcs(training_records, validation_records)
+  if gcs_upload:
+    upload_to_gcs(training_records=training_records,
+                  validation_records=validation_records,
+                  gcs_output_path=gcs_output_path,
+                  gcs_project=gcs_project,
+                  client=client)
+
+
+def main(_):
+  run(raw_data_dir=FLAGS.raw_data_dir,
+      gcs_upload=FLAGS.gcs_upload,
+      gcs_project=FLAGS.project,
+      gcs_output_path=FLAGS.gcs_output_path,
+      local_scratch_dir=FLAGS.local_scratch_dir)
 
 
 if __name__ == '__main__':
+  logging.set_verbosity(logging.INFO)
+  tf.disable_v2_behavior()
   app.run(main)
